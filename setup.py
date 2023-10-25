34fb460b7dc428214742ce33f2254777c564b6cc
Kanitta Srichan <uay3yb@virginia.edu>
2023-10-24 20:26:18 -0400

    add files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'mypackage',
+    version = '1.0',
+    author = 'Kanitta Srichan',
+    author_email = 'uay3yb@virginia.com',
+    description = "An awsome poeject" 
+    url = 'https://github.com/KanittaSrichan/My_project',
+    license = 'MIT',
+    package = ['mypackage']
+)
